069a20adf6698ff44e94026983d5c080fb26248f
tmccafferty03 <21326992@stu.mmu.ac.uk>
2023-05-04 14:30:04 +0100

    update

---
+++ vue-project/src/services/match.service.js
@@ -0,0 +1,92 @@
+const createMatch = (data) => {
+    return fetch("http://localhost:3333/match", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": localStorage.getItem("session_token")
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(response.status === 201){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const updateMatch = (match_id, data) => {
+    return fetch("http://localhost:3333/match/" + match_id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": localStorage.getItem("session_token")
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const getAllMatches = (tournament_id) => {
+    return fetch("http://localhost:3333/tournament/" + tournament_id + "/comments")
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const getOneMatch = (match_id) => {
+    return fetch("http://localhost:3333/match/" + match_id)
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+export const articleService = {
+    createMatch,
+    updateMatch,
+    getAllMatches,
+    getOneMatch
+}
---
+++ vue-project/src/services/user.service.js
@@ -0,0 +1,152 @@
+const createUser = (data) => {
+    return fetch("http://localhost:3333/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": localStorage.getItem("session_token")
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(response.status === 201){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const getOneUser = (user_id) => {
+    return fetch("http://localhost:3333/users/" + user_id)
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const getAllUsers = () => {
+    return fetch("http://localhost:3333/users", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": localStorage.getItem("session_token")
+        }
+    })
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const updateUser = (user_id, data) => {
+    return fetch("http://localhost:3333/user/" + user_id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": localStorage.getItem("session_token")
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .then((resJson) => {
+        return resJson
+    })
+    .catch((error) => {
+        console.log("Err", error)
+        return Promise.reject(error)
+    })
+}
+
+const login = (data) => {
+    return fetch("http://localhost:3333/login", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }else if(response.status === 400){
+            throw "Bad data was sent to the server";
+        }else{
+            alert(response.status)
+            throw "Something went wrong";
+        }
+    })
+    .then((resJson) => {
+        localStorage.setItem("user_id", resJson.user_id);
+        localStorage.setItem("session_token", resJson.session_token)
+        return resJson
+    })
+    .catch((error) => {
+        alert(error);
+    })
+}
+
+const logout = () => {
+    return fetch("http://localhost:3333/logout", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": localStorage.getItem("session_token")
+        }
+    })
+    .then((response) => {
+        if(response.status === 200){
+            localStorage.removeItem("user_id")
+            localStorage.removeItem("session_token")
+            return
+        }else if(response.status === 401){
+            throw "Not logged in"
+        }else{
+            throw "Something went wrong"
+        }
+    })
+    .catch((error) => {
+        alert(error)
+    })
+}
+
+export const userService = {
+    createUser,
+    getOneUser,
+    getAllUsers,
+    updateUser,
+    login,
+    logout
+}
